b76e93f03265273c5b5b6a012897cbe326656d4d
Tamemask <tamemask@gmail.com>
2020-04-01 11:27:38 +0300

    amount of numbers added for both progressions, step of progression added for arifmetic

--- src/ru/innopolis/stc/java/lesson4/task4/Progression.java
+++ src/ru/innopolis/stc/java/lesson4/task4/Progression.java
@@ -7,20 +7,28 @@ public class Progression {
         Scanner in = new Scanner(System.in);
         int i;
         int j;
+        int n;
+        int step;
         System.out.println("Введите 1, если хотите арифметическую прогрессию \nВведите 2, если хотите геометрическую прогрессию");
         i = in.nextInt();
         System.out.println("Введите число, для которого хотите прогрессию");
         j = in.nextInt();
+        System.out.println("Введите N (количество чисел в прогрессии)");
+        n = in.nextInt();
+        System.out.println("Введите шаг прогрессии");
+        step = in.nextInt();
         int f = 0;
         int z = 1;
         switch (i) {
             case 1:
-                for (int s = 0; s < 10; s++) {
-                    System.out.println(f += j);
+                System.out.println(j);
+                for (int s = 1; s < n; s++) {
+                    System.out.println(f = f + step*j);
+                    step++;
                 }
                 break;
             case 2:
-                for (int s = 0; s < 10; s++) {
+                for (int s = 0; s < n; s++) {
                     System.out.println(z *= j);
                 }
                 break;
